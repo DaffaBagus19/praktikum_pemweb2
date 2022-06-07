fe9151472dec8cce6fce624b56eb53cd08b3992b
DaffaBagus19 <daffaradityo249@gmail.com>
2022-06-08 06:47:47 +0700

    Mengupload Tugas

--- pratikum_13/application/config/config.php
+++ pratikum_13/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/php/praktikum_13/';
+$config['base_url'] = 'http://localhost/praktikum_pemweb2/pratikum_13/';
 
 /*
 |--------------------------------------------------------------------------
